fa30660b36e27ab11f1dc6e1f7089c326e99c7e0
Risbaev <risbaevshaxzat3@gmail.com>
2023-02-22 12:51:06 +0500

    first commit

---
+++ src/main/java/com/company/kinotime/KinoTimeApplication.java
@@ -0,0 +1,13 @@
+package com.company.kinotime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoTimeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/company/kinotime/entity/Product.java
@@ -0,0 +1,2 @@
+package com.company.kinotime.entity;public class Product {
+}
